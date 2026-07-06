0a10e89cdb9dc89dd09ed7d016da2421869176bf
neerajjha0426 <114871274+neerajjha0426@users.noreply.github.com>
2026-07-06 15:03:27 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (60.13%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.cpp
+++ 1222-remove-covered-intervals/remove-covered-intervals.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
         
+        //sorting on basis of 0th and 1st index
         sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) {
             if (a[0] == b[0]) return a[1] > b[1]; 
             return a[0] < b[0];
@@ -9,7 +10,7 @@ public:
 
         int count = 0;
         int maxEnd = 0;
-
+        //simple hai agar kisi ka end alreday ho jo chika hai like 1 bje tk meeting schedule hai to kisi aur ko kuch nhi hoga tb tk 
         for (auto& it : intervals) {
             if (it[1] > maxEnd) {
                 count++;               // not covered
